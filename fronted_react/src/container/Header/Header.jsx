aa7f0d79c3c890dcf3d75864f4b9244df426fa13
mouloud <mouloud.elasri.123@gmail.com>
2022-04-11 00:26:55 +0000

    start header

--- fronted_react/src/container/Header/Header.jsx
+++ fronted_react/src/container/Header/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { motion } from 'framer-motion';
 
+import { images } from '../../constants';
 import './Header.scss';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="app_header app__flex">
+      <motion.div 
+        whileInView={{ x: [-100,0], opacity: [0,1]}}
+        transition={{ duration: 0.5}}
+        className="app_header-info"
+      >
+        <div className="app_header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋 </span>
+            <p className="p-text">Hello, I am</p>
+            <h1 className="head-text">Elasri Mouloud</h1>
+          </div>
+
+        </div>
+
+      </motion.div>
+    </div>   
   )
 }
 
